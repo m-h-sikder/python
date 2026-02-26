459de7967eb24e3ecc9f259c764473620870cfe5
Monir Sikder <61249536+m-h-sikder@users.noreply.github.com>
2026-02-26 16:17:48 -0500

    Add files via upload

---
+++ comparison_logical_practice.py
@@ -0,0 +1,15 @@
+"""
+Topic: Comparison & Logical Operators
+"""
+
+x = 25
+y = 2
+
+print("x >= y:", x >= y)
+print("x <= y:", x <= y)
+print("x == y:", x == y)
+print("x != y:", x != y)
+
+print("x > 5 and x < 30:", x > 5 and x < 30)
+print("x > 5 or x < 2:", x > 5 or x < 2)
+print("not x > 7:", not x > 7)
